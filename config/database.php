2f9b2024b3b92392dddc95e3d34bb51085ec7b36
GNT-GLOBAL\minh.pc <minh.pc@vn.gnt-global.com>
2018-03-20 16:27:27 +0700

    change connection

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('PG_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('PG_DATABASE', 'forge'),
-            'username' => env('PG_USERNAME', 'forge'),
-            'password' => env('PG_PASSWORD', ''),
+            'host' => env('PG_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('PG_DATABASE'),
+            'username' => env('PG_USERNAME'),
+            'password' => env('PG_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
