60bd5251beeb0a584987566dcb8f5abcc5b69348
NeoHuy <quanghuy9490@gmail.com>
2018-03-23 22:43:34 +0700

    Recreating minimo layout 95%

--- public/minimo/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".open-hamburger-menu").click(function() {
-        if( $(".hamburger-menu").is(":visible") ) {
-            $(".hamburger-menu").hide();
-        } else {
-            $(".hamburger-menu").show();
-        }
-    });
-});
